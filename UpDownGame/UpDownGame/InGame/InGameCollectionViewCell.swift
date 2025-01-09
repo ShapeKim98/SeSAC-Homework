2b06f51f11f30d4ccb5c86566a094c985a3df0c5
김도형 <shapekim98@gmail.com>
2025-01-09 16:56:07 +0900

    [feat] #11 InGame 뷰컨트롤러, 셀 추가

---
+++ UpDownGame/UpDownGame/InGame/InGameCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  InGameCollectionViewCell.swift
+//  UpDownGame
+//
+//  Created by 김도형 on 1/9/25.
+//
+
+import UIKit
+
+class InGameCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ UpDownGame/UpDownGame/InGame/InGameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InGameViewController.swift
+//  UpDownGame
+//
+//  Created by 김도형 on 1/9/25.
+//
+
+import UIKit
+
+class InGameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
