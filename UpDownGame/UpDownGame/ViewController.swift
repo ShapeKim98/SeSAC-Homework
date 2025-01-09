29c89d87e88380f69e0343dfa23dba03757cb806
김도형 <shapekim98@gmail.com>
2025-01-09 16:46:29 +0900

    [feat] #11 프로젝트 생성 및 홈화면, 인게임 화면 구성

---
+++ UpDownGame/UpDownGame/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  UpDownGame
+//
+//  Created by 김도형 on 1/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet
+    private var numberTextField: UITextField!
+    @IBOutlet
+    private var gameImageView: UIImageView!
+    @IBOutlet
+    private var tryCountLabel: UILabel!
+    @IBOutlet
+    private var resultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
+
+// MARK: View
+private extension ViewController {
+    func configureResultLabel() {
+        
+    }
+}
