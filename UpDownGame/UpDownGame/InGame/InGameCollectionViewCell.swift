c609f2689ab65a7b36d4b8915b685ba5ae76cbb9
김도형 <shapekim98@gmail.com>
2025-01-09 18:49:58 +0900

    [feat] #11 인게임 셀 구현

--- UpDownGame/UpDownGame/InGame/InGameCollectionViewCell.swift
+++ UpDownGame/UpDownGame/InGame/InGameCollectionViewCell.swift
@@ -8,10 +8,40 @@
 import UIKit
 
 class InGameCollectionViewCell: UICollectionViewCell {
-
+    @IBOutlet
+    private var numberLabel: UILabel!
+    @IBOutlet
+    private var numberBackgroundView: UIView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        configureNumberLabel()
     }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        configureBackgroundView()
+    }
+}
 
+// MARK: View
+private extension InGameCollectionViewCell {
+    func configureBackgroundView() {
+        numberBackgroundView.backgroundColor = .white
+        let width = numberBackgroundView.frame.width
+        
+        numberBackgroundView.layer.cornerRadius = width / 2
+    }
+    
+    func configureNumberLabel() {
+        numberLabel.textColor = .black
+        numberLabel.font = .systemFont(ofSize: 12)
+    }
+}
+
+extension String {
+    static let inGameCollectionCell = "InGameCollectionViewCell"
 }
