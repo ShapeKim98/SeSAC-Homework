37f354c6347080939f2be2a9e0a1540dfa26b2c3
김도형 <shapekim98@gmail.com>
2025-01-10 17:35:34 +0900

    [feat] #13 TravelTalk 프로젝트 생성

---
+++ TravelTalk/TravelTalk/Home/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelTalk
+//
+//  Created by 김도형 on 1/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
