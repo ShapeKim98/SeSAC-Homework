a99deaba7af976e515a6826399c2bcb0e6790bd5
김도형 <shapekim98@gmail.com>
2025-01-14 00:43:45 +0900

    [refactor] #15 AsyncStream으로 코드 정리

--- ChangeTeam.playground/Contents.swift
+++ ChangeTeam.playground/Contents.swift
@@ -9,37 +9,42 @@ import UIKit
 
 func 돌려돌려돌림판() {
     // 기존 팀별 좌석 배치
+    var 팀좌석 = [1, 2, 3, 4, 5]
+    for i in 0..<5 {
+        print("\(i + 1)팀의 좌석은 \(팀좌석[i]) 입니다.")
+    }
+    
+    /*
+     기존의 팀이 동일한 자리에 앉지 않도록 랜덤하게 팀좌석을 바꿔주세요
+     하단 출력 형태에 맞게 동작하도록, 코드를 이곳에 작성해주세요.
+     */
+    let rollerCount = 70
+    let sleepStream = AsyncStream<Int> { continuation in
+        Task {
+            for i in 1..<(rollerCount + 1) {
+                try? await Task.sleep(nanoseconds: UInt64(100000 * i * i))
+                continuation.yield(i)
+            }
+            continuation.finish()
+        }
+    }
     
     Task {
-        var 팀좌석 = [1, 2, 3, 4, 5]
-        for i in 0..<5 {
-            print("\(i + 1)팀의 좌석은 \(팀좌석[i]) 입니다.")
-        }
-        
-        /*
-         기존의 팀이 동일한 자리에 앉지 않도록 랜덤하게 팀좌석을 바꿔주세요
-         하단 출력 형태에 맞게 동작하도록, 코드를 이곳에 작성해주세요.
-         */
-        
-        for i in 0..<70 {
-            try? await Task.sleep(nanoseconds: UInt64(100000 * i * i))
-            
+        for await count in sleepStream {
             while (isNotValid(팀좌석: &팀좌석)) { }
             
-            print("\n\n\n\n\n\n")
+            print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
             print("---------------------------------------")
             for i in 0..<5 {
                 print("\(i + 1)팀의 좌석은 \(팀좌석[i]) 입니다.")
             }
             print("---------------------------------------")
-            if i == 69 {
+            if count == 70 {
                 print("좌석 돌리기 완료!")
             } else {
                 print("좌석 돌리는 중...")
             }
-            print("\n\n\n\n\n\n")
         }
-        
     }
 }
 
