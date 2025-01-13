159dc4e295526aa590b60fa2549b1818c49231a6
김도형 <shapekim98@gmail.com>
2025-01-13 16:32:04 +0900

    [feat] #13 SnapKitPractice 프로젝트 생성

---
+++ SnapKitPractice/SnapKitPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapKitPractice
+//
+//  Created by 김도형 on 1/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
