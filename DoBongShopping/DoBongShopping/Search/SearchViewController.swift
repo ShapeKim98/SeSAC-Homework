e473ffe10b86798246dfe4c1b0d21f3418ee5ff1
김도형 <shapekim98@gmail.com>
2025-01-15 17:12:57 +0900

    [design] #19 검색화면 구성

--- DoBongShopping/DoBongShopping/Search/SearchViewController.swift
+++ DoBongShopping/DoBongShopping/Search/SearchViewController.swift
@@ -7,9 +7,59 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+import SnapKit
 
+class SearchViewController: UIViewController {
+    let searchBar = UISearchBar()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureUI()
+        
+        configureLayout()
     }
+}
+
+//MARK: Configure View
+private extension SearchViewController {
+    func configureUI() {
+        view.backgroundColor = .black
+        
+        configureNavigtion()
+        
+        configureSearchBar()
+    }
+    
+    func configureLayout() {
+        searchBar.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.horizontalEdges.equalToSuperview().inset(16)
+        }
+    }
+    
+    func configureNavigtion() {
+        navigationItem.title = "도봉러의 쇼핑쇼핑"
+        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
+    }
+    
+    func configureSearchBar() {
+        searchBar.delegate = self
+        searchBar.placeholder = "브랜드, 상품, 프로필, 태그 등"
+        searchBar.barTintColor = .black
+        searchBar.barStyle = .black
+        searchBar.searchTextField.backgroundColor = .secondaryLabel
+        searchBar.searchTextField.textColor = .white
+        view.addSubview(searchBar)
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+    }
+}
+
+#Preview {
+    UINavigationController(rootViewController: SearchViewController())
 }
