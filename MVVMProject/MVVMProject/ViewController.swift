67e4a2da80d2456403479f1ae514a603d612ac0a
김도형 <shapekim98@gmail.com>
2025-02-05 20:00:34 +0900

    [feat] #27 3가지 기능 탭바 연결

--- MVVMProject/MVVMProject/ViewController.swift
+++ MVVMProject/MVVMProject/ViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        configureTabBarController()
+        
+        configureTabBarAppearance()
     }
-
-
+    
+    private func configureTabBarController() {
+        let currencyViewController = UINavigationController(rootViewController: CurrencyViewController())
+        currencyViewController.tabBarItem.title = "환전"
+        currencyViewController.tabBarItem.image = UIImage(systemName: "dollarsign")
+        
+        let wordCounterViewController = UINavigationController(rootViewController: WordCounterViewController())
+        wordCounterViewController.tabBarItem.title = "검색"
+        wordCounterViewController.tabBarItem.image = UIImage(systemName: "pencil")
+        
+        let userViewController = UINavigationController(rootViewController: UserViewController())
+        userViewController.tabBarItem.title = "검색"
+        userViewController.tabBarItem.image = UIImage(systemName: "person")
+        
+        setViewControllers(
+            [currencyViewController, wordCounterViewController, userViewController],
+            animated: true
+        )
+    }
+    
+    private func configureTabBarAppearance() {
+        let appearance = UITabBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .systemBackground
+        tabBar.standardAppearance = appearance
+        tabBar.scrollEdgeAppearance = appearance
+        tabBar.tintColor = .black
+        UITabBar.appearance().scrollEdgeAppearance = appearance
+    }
 }
 
