0b5044c09a647a6ad828b303e238661ea25d452c
김도형 <shapekim98@gmail.com>
2025-02-05 16:57:31 +0900

    [feat] #27 프로젝트 생성

---
+++ MVVMProject/MVVMProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMProject
+//
+//  Created by 김도형 on 2/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
