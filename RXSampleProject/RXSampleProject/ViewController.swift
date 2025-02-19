674f6031731dd6aa7b3b0eb801d7889cd777b2a2
김도형 <shapekim98@gmail.com>
2025-02-19 16:53:04 +0900

    [fea] #31 루트 뷰 추가

--- RXSampleProject/RXSampleProject/ViewController.swift
+++ RXSampleProject/RXSampleProject/ViewController.swift
@@ -7,13 +7,90 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import SnapKit
 
+class ViewController: UIViewController {
+    private lazy var tableButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Simple Table", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.addAction(
+            UIAction { [weak self] _ in
+                self?.pushTableViewController()
+            },
+            for: .touchUpInside
+        )
+        return button
+    }()
+    private lazy var validationButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Simple Validation", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.addAction(
+            UIAction { [weak self] _ in
+                self?.pushValidationViewController()
+            },
+            for: .touchUpInside
+        )
+        return button
+    }()
+    private lazy var numbersButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Simple Numbers", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.addAction(
+            UIAction { [weak self] _ in
+                self?.pushNumbersViewController()
+            },
+            for: .touchUpInside
+        )
+        return button
+    }()
+    private lazy var birthdayButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Simple Birthday", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.addAction(
+            UIAction { [weak self] _ in
+                self?.pushBirthdayViewController()
+            },
+            for: .touchUpInside
+        )
+        return button
+    }()
+    private lazy var vstack: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [tableButton, validationButton, numbersButton, birthdayButton])
+        stack.axis = .vertical
+        stack.spacing = 32
+        stack.distribution = .fillEqually
+        view.addSubview(stack)
+        return stack
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemBackground
+        
+        vstack.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
     }
 
-
+    private func pushTableViewController() {
+        navigationController?.pushViewController(SimpleTableViewController(), animated: true)
+    }
+    
+    private func pushValidationViewController() {
+        navigationController?.pushViewController(SimpleValidationViewController(), animated: true)
+    }
+    
+    private func pushNumbersViewController() {
+        navigationController?.pushViewController(NumbersViewController(), animated: true)
+    }
+    
+    private func pushBirthdayViewController() {
+        navigationController?.pushViewController(BirthdayViewController(), animated: true)
+    }
 }
 
