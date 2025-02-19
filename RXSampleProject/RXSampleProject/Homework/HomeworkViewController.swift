ec3ab6491e3c6a02176ae827594894797aee54e0
김도형 <shapekim98@gmail.com>
2025-02-19 23:37:20 +0900

    [feat] #33 HomeworViewController 화면 및 로직 구성

---
+++ RXSampleProject/RXSampleProject/Homework/HomeworkViewController.swift
@@ -0,0 +1,118 @@
+//
+//  HomeworkViewController.swift
+//  RxSwift
+//
+//  Created by Jack on 1/30/25.
+//
+
+import UIKit
+
+import Kingfisher
+import SnapKit
+import RxSwift
+import RxCocoa
+
+struct Person: Identifiable {
+    let id = UUID()
+    let name: String
+    let email: String
+    let profileImage: String
+}
+
+class HomeworkViewController: UIViewController {
+    typealias Action = HomeworkViewModel.Action
+    
+    let tableView = UITableView()
+    lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout())
+    let searchBar = UISearchBar()
+    
+    private let viewModel = HomeworkViewModel()
+    
+    private let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+        bind()
+    }
+     
+    private func bind() {
+        bindSampleUsers()
+        
+        bindSelectedUsers()
+    }
+    
+    private func bindSampleUsers() {
+        viewModel.bind
+            .map(\.sampleUsers)
+            .bind(to: tableView.rx.items(
+                cellIdentifier: PersonTableViewCell.identifier,
+                cellType: PersonTableViewCell.self
+            )) { row, elements, cell in
+                cell.usernameLabel.text = elements.name
+                let url = URL(string: elements.profileImage)
+                cell.profileImageView.kf.setImage(with: url)
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    private func bindSelectedUsers() {
+        viewModel.bind
+            .map(\.selectedUsers)
+            .bind(to: collectionView.rx.items(
+                cellIdentifier: UserCollectionViewCell.identifier,
+                cellType: UserCollectionViewCell.self
+            )) { item, elements, cell in
+                cell.label.text = elements.name
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    private func configure() {
+        view.backgroundColor = .white
+        view.addSubview(tableView)
+        view.addSubview(collectionView)
+        view.addSubview(searchBar)
+        
+        navigationItem.titleView = searchBar
+        searchBar.rx.searchButtonClicked
+            .withLatestFrom(searchBar.rx.text.orEmpty)
+            .map { Action.searchButtonClicked($0.lowercased()) }
+            .bind(to: viewModel.send)
+            .disposed(by: disposeBag)
+         
+        collectionView.register(UserCollectionViewCell.self, forCellWithReuseIdentifier: UserCollectionViewCell.identifier)
+        collectionView.backgroundColor = .lightGray
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.horizontalEdges.equalToSuperview()
+            make.height.equalTo(50)
+        }
+        
+        tableView.register(PersonTableViewCell.self, forCellReuseIdentifier: PersonTableViewCell.identifier)
+        tableView.backgroundColor = .systemGreen
+        tableView.rowHeight = 100
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(50)
+            make.horizontalEdges.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+        
+        tableView.rx.itemSelected
+            .map { Action.tableViewItemSelected($0.row) }
+            .bind(to: viewModel.send)
+            .disposed(by: disposeBag)
+    }
+    
+    private func layout() -> UICollectionViewFlowLayout {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 80, height: 40)
+        layout.scrollDirection = .horizontal
+        return layout
+    }
+
+}
+ 
+#Preview {
+    UINavigationController(rootViewController: HomeworkViewController())
+}
