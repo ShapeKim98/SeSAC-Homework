e0e907e36764b7134c1c73975aa438edb4aa8b91
김도형 <shapekim98@gmail.com>
2025-02-25 01:30:10 +0900

    [refactor] #37 Effect 구조 변경

--- SnapKitPractice/SnapKitPractice/Lotto/LottoViewModel.swift
+++ SnapKitPractice/SnapKitPractice/Lotto/LottoViewModel.swift
@@ -44,7 +44,7 @@ final class LottoViewModel: Composable {
             .withUnretained(self)
             .map { this, action in
                 var state = this.state.value
-                this.reducer(&state, action)
+                this.reducer(&state, action).observable
                     .compactMap(\.action)
                     .bind(to: this.send)
                     .disposed(by: this.disposeBag)
@@ -54,7 +54,7 @@ final class LottoViewModel: Composable {
             .disposed(by: disposeBag)
     }
     
-    private func reducer(_ state: inout State, _ action: Action) -> Observable<Effect<Action>> {
+    private func reducer(_ state: inout State, _ action: Action) -> Effect<Action> {
         switch action {
         case .viewDidLoad:
             let firstLottery = "2002-12-07"
@@ -79,7 +79,10 @@ final class LottoViewModel: Composable {
             let request = LottoRequest(drwNo: "\(lotteryDay)")
             let fetchLottoSingle = lottoClient.fetchLottoSingle(request)
             return .run(fetchLottoSingle.map { .bindLotto($0) }) { error in
-                return .none
+                guard let error = error as? LottoError else {
+                    return .none
+                }
+                return .send(.bindError(error))
             }
         case let .bindLotto(lotto):
             state.lotto = lotto
