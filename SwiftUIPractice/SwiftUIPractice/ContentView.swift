278edab0085f614870ec4aaf1c3505d8ec57eea3
김도형 <shapekim98@gmail.com>
2025-04-15 19:17:11 +0900

    [feat] #45 탭바 연결

--- SwiftUIPractice/SwiftUIPractice/ContentView.swift
+++ SwiftUIPractice/SwiftUIPractice/ContentView.swift
@@ -8,14 +8,89 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State
+    private var selectedTab: Tab = .home
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView(selection: $selectedTab) {
+            switch selectedTab {
+            case .home:
+                HomeView()
+                    .toolbarBackground(.hidden, for: .tabBar)
+            case .mission:
+                MissionView()
+                    .toolbarBackground(.hidden, for: .tabBar)
+            case .randomImage:
+                NavigationStack {
+                    MyRandomImageView()
+                        .toolbarBackground(.hidden, for: .tabBar)
+                }
+            }
         }
-        .padding()
+        .overlay(alignment: .bottom) {
+            tabBar
+        }
+        .ignoresSafeArea()
+        .preferredColorScheme(.dark)
+    }
+    
+    enum Tab: CaseIterable {
+        case home
+        case mission
+        case randomImage
+        
+        var title: String {
+            switch self {
+            case .home: return "홈"
+            case .mission: return "혜택"
+            case .randomImage: return "랜덤 이미지"
+            }
+        }
+        
+        var image: String {
+            switch self {
+            case .home: return "house.fill"
+            case .mission: return "diamond.fill"
+            case .randomImage: return "photo.fill"
+            }
+        }
+    }
+}
+
+private extension ContentView {
+    @ViewBuilder
+    func tabItem(_ tab: Tab) -> some View {
+        let isSelected = selectedTab == tab
+        
+        Button {
+            selectedTab = tab
+        } label: {
+            VStack(spacing: 8) {
+                Image(systemName: tab.image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 24, height: 24)
+                    .foregroundStyle(isSelected ? .white : .secondary)
+                
+                Text(tab.title)
+                    .font(.subheadline)
+                    .foregroundStyle(isSelected ? .white : .secondary)
+            }
+            .frame(maxWidth: .infinity)
+        }
+    }
+    
+    var tabBar: some View {
+        HStack {
+            ForEach(Tab.allCases, id: \.hashValue) { tab in
+                tabItem(tab)
+            }
+        }
+        .padding(.top, 8)
+        .padding(.bottom, 28)
+        .padding(.horizontal, 16)
+        .background(Color(red: 0.1, green: 0.1, blue: 0.1))
+        .roundedCorner(20, corners: [.topLeft, .topRight])
     }
 }
 
---
+++ SwiftUIPractice/SwiftUIPractice/RoundedCorner.swift
@@ -0,0 +1,28 @@
+//
+//  RoundedCorner.swift
+//  SwiftUIPractice
+//
+//  Created by 김도형 on 4/15/25.
+//
+
+import SwiftUI
+
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(
+            roundedRect: rect,
+            byRoundingCorners: corners,
+            cornerRadii: CGSize(width: radius, height: radius)
+        )
+        return Path(path.cgPath)
+    }
+}
+
+extension View {
+    func roundedCorner(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedCorner(radius: radius, corners: corners) )
+    }
+}
